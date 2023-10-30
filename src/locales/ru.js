a4ce6dd19c0e75d5cf7020bbd6cb3ea1ce0c0252
Svetlna <svetlana@dev.com>
2023-10-30 15:47:34 +0300

    created locales

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    heading: 'RSS агрегатор',
+    tagline: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    input: 'Ссылка RSS',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    submitBtn: 'Добавить',
+    errors: {
+      validation: {
+        url: 'Ссылка должна быть валидным URL',
+        notOneOf: 'RSS уже существует',
+      },
+    },
+
+  },
+};
