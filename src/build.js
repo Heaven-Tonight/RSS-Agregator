cabc9666fda4f1bbef77f17ca129ddc5e59d3c0c
Svetlna <svetlana@dev.com>
2023-10-31 14:12:48 +0300

    created build.js

---
+++ src/build.js
@@ -0,0 +1,20 @@
+export default (html, feedId) => {
+  const posts = [];
+  const feed = {
+    title: html.querySelector('title').textContent,
+    description: html.querySelector('description').textContent,
+    id: feedId,
+  };
+
+  const items = html.querySelectorAll('item');
+  items.forEach((item, index) => {
+    posts.push({
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').nextSibling.textContent.trim(),
+      id: index,
+      feedId: feed.id,
+    });
+  });
+  return { feed, posts };
+};
