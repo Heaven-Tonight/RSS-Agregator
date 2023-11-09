59ad5d25b9a70fad844247254c84b137198d5c1b
Svetlna <svetlana@dev.com>
2023-11-09 20:34:47 +0300

    updated app.js

--- src/app.js
+++ src/app.js
@@ -107,6 +107,7 @@ export default async () => {
       watchedState.form.process = 'submitting';
       const formData = new FormData(e.target);
       const rss = formData.get('url');
+      console.log('RSS', rss)
 
       schema.validate({ url: rss }, { abortEarly: false })
         .then(({ url }) => {
@@ -129,6 +130,7 @@ export default async () => {
               updateRssStream(watchedState);
             })
             .catch((err) => {
+              console.log('ERROR', err)
               watchedState.feeds.urlList.pop();
               if (err.message === 'invalid RSS') {
                 watchedState.form.error = { key: 'errors.rssError' };
