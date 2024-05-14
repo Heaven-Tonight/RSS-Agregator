a337521fc2bd8777e6a05909f28ea8e4469e02a2
svetlana <svetlanaa.anisimova@yandex.ru>
2024-05-14 14:29:30 +0300

    refactored code

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,4 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -19,9 +18,6 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
@@ -48,9 +44,6 @@ const config = {
                 test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
                 use: 'file-loader',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
 };
@@ -58,8 +51,8 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
+
+
     } else {
         config.mode = 'development';
     }
